1ae5afb4fe1d07c2eae81355fd16ca3998cb94e2
Sergrs <sergeyrs@bk.ru>
2021-06-07 05:50:49 +0300

    Save in remote repo

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from leads.views import landing_page, LandingPageView
+from leads.views import landing_page, LandingPageView,SignUpView
+from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPageView.as_view(), name="landing_page"),
-    path('leads/', include('leads.urls', namespace="leads"))
+    path('', LandingPageView.as_view(), name="landing-page"),
+    path('leads/', include('leads.urls', namespace="leads")),
+    path('login/', LoginView.as_view(), name="login"),
+    path('signup/', SignUpView.as_view(), name="signup"),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
+
+
+class SignUpView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+    
+    def get_success_url(self):
+        return reverse("login")
+
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
@@ -45,6 +55,16 @@ class LeadCreateView(generic.CreateView):
     
     def get_success_url(self):
         return reverse("leads:lead-list")
+
+    def form_valid(self, form):
+        # TO DO send e-mail
+        send_mail(
+            subject="A lead has been created", 
+            message="Go to the site to see the new lead", 
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
 
 
 
