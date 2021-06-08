4edd7fc0a007a090111fb0faaab82255bed92261
Sergrs <sergeyrs@bk.ru>
2021-06-08 21:10:53 +0300

    save

--- agents/views.py
+++ agents/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_list = "agents/agent_list.html"
