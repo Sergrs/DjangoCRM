2625ec14e551e97344d7fd33769cb94b99b6417c
Sergrs <sergeyrs@bk.ru>
2021-06-08 05:34:28 +0300

    Another day

--- leads/admin.py
+++ leads/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.core.handlers.wsgi import LimitedStream
 
-from .models import User, Lead, Agent
+from .models import User, Lead, Agent, UserProfile
 
 admin.site.register(User)
 admin.site.register(Lead)
 admin.site.register(Agent)
+admin.site.register(UserProfile)
